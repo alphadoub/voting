d2a529e2ca5f2366f0482ccf1934ccac705dd454
alphadoub <core2mind@gmail.com>
2018-01-07 00:52:39 +0600

    add logging: slf4j + logback

--- src/test/java/ru/alphadoub/voting/service/AbstractServiceTest.java
+++ src/test/java/ru/alphadoub/voting/service/AbstractServiceTest.java
@@ -1,24 +1,56 @@
 package ru.alphadoub.voting.service;
 
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.alphadoub.voting.validation.ValidationUtil;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 @ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = "classpath:database/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public abstract class AbstractServiceTest {
+    private static final Logger log = LoggerFactory.getLogger("result");
+
+    private static StringBuilder results = new StringBuilder();
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @Rule
+    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            log.info(result + " ms\n");
+        }
+    };
+
+    @AfterClass
+    public static void printResult() {
+        log.info("\n-------------------------------------------------------------------------------------------------------" +
+                "\nTest                                                                                       Duration, ms" +
+                "\n-------------------------------------------------------------------------------------------------------\n" +
+                results +
+                "-------------------------------------------------------------------------------------------------------\n");
+        results.setLength(0);
+    }
 
     public static <T extends Throwable> void validateRootCause(Runnable runnable, Class<T> exceptionClass) {
         try {
@@ -28,5 +60,4 @@ public abstract class AbstractServiceTest {
             Assert.assertThat(ValidationUtil.getRootCause(e), instanceOf(exceptionClass));
         }
     }
-
 }
